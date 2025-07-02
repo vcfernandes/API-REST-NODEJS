209603060483376ff6f6400e204005dcd8048c81
Vinicius de Carvalho Fernandes <114537856+vcfernandes@users.noreply.github.com>
2025-07-02 03:55:25 -0300

    fix1.0: Production config

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,9 @@ import {app} from './app'
 import { env } from './env'
 
 app.listen({
-    port: env.PORT,
-  })
-  .then(() => { 
-    console.log('HTTP Server Running!')
-  })
+  host: '0.0.0.0',
+  port: env.PORT,
+})
+.then(() => {
+  console.log('HTTP Server Running!')
+})
